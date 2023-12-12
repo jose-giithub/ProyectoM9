334f48a3049fbe4c673a94eb474b594e8f421e2d
Daxter89 <daxterxavi@gmail.com>
2023-12-12 21:24:20 +0100

    Creadas las migraciones

---
+++ database/migrations/datos_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('datos', function (Blueprint $table) {
+            $table->id();
+            $table->string('nombre');
+            $table->string('precio');
+            $table->date('fecha_inicio');
+            $table->date('fecha_final');
+            $table->binary('imagen');
+            // Otras columnas y restricciones aquí
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('nombre_de_la_tabla');
+    }
+};
