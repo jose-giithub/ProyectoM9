12888adecfd738e41016466358ea9016ca98fd93
jose-giithub <jose.rodriguez@insbaixcamp.cat>
2024-01-14 19:53:06 +0100

    Creo la vista con 3 img

--- resources/views/auth/destinaciones.blade.php
+++ resources/views/auth/destinaciones.blade.php
@@ -1,28 +1,30 @@
 <!DOCTYPE html>
 <html lang="es">
+
 <head>
     <meta charset="UTF-8">
-    <title>Lista de destinos</title>
+    <title>Detalle del Destino</title>
+    <style>
+        img {
+            max-width: 30%;
+            height: auto;
+        }
+    </style>
 </head>
+
 <body>
     <div class="container">
-        <h1>Lista de destinos</h1>
-
-        <div class="row">
-            @foreach($destinations as $destination)
-                <div class="col-md-4">
-                    <div class="card">
-                    <img class="card-img-top" src="{{ asset('images/molinos.jpg') }}" alt="Imagen de {{ $destination->name }}">
-
-                        <div class="card-body">
-                            <h5 class="card-title">{{ $destination->name }}</h5>
-                            <p class="card-text">{{ $destination->description }}</p>
-                            <a href="#" class="btn btn-primary">Ver detalle</a>
-                        </div>
-                    </div>
-                </div>
-            @endforeach
+        <h1>Detalle del Destino</h1>
+        <div id="img1">
+        <img src="{{ asset('images/maspullols.jpg') }}" alt="Imagen de destino">
+        </div>
+        <div id="img2">
+        <img src="{{ asset('images/leon.jpg') }}" alt="Imagen de destino">
+        </div>
+        <div id="img3">
+        <img src="{{ asset('images/galicia.jpg') }}" alt="Imagen de destino">
         </div>
     </div>
 </body>
+
 </html>
