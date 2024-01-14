a34911f3bed039778d9a629bdab3c7743f1d37b7
Daxter89 <daxterxavi@gmail.com>
2024-01-14 18:45:21 +0100

    Fucsió de la branca Migrations a la Main

---
+++ database/migrations/destinos_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('destinos', function (Blueprint $table) {
+            $table->id();
+            $table->string('localizacion');
+            $table->string('destino');
+            $table->binary('imajen');
+            // Otras columnas y restricciones aquí
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('nombre_de_la_tabla');
+    }
+};
