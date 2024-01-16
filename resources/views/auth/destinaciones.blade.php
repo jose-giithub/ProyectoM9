ef4f122c9dce98a064d16e04b0e040babbd3c3b7
Daxter89 <daxterxavi@gmail.com>
2024-01-16 15:40:39 +0100

    Cambio de css
    Cambio de fichero css separado.

--- resources/views/auth/destinaciones.blade.php
+++ resources/views/auth/destinaciones.blade.php
@@ -4,49 +4,7 @@
 <head>
     <meta charset="UTF-8">
     <title>Detalle del Destino</title>
-    <style>
-        body{
-            background-color: #BBDEFB;
-        }
-        header {
-            text-align: center;
-            margin-bottom: 2rem;
-           
-        }
-
-        .container {
-            display: flex;
-            justify-content: space-around;
-            flex-wrap: wrap;
-            background-color: #BBDEFB;
-        }
-
-        .card {
-            margin: 1rem;
-            border: 1px solid #ddd;
-            border-radius: 4px;
-            box-shadow: 0 2px 4px rgba(0, 0, 0, 0.1);
-            overflow: hidden;
-            width: 300px;
-            /* Ancho fijo para cada tarjeta */
-            height: 800px;
-            /* Altura fija para cada tarjeta */
-            background-color: #FFE57F;
-        }
-
-        .card img {
-            width: 100%;
-            height: 600px;
-            /* Altura fija para las imágenes */
-            object-fit: cover;
-            /* Asegura que la imagen cubra el área sin distorsionarse */
-        }
-
-        .card-body {
-            padding: 1rem;
-            text-align: center;
-        }
-    </style>
+    <link rel="stylesheet" href="{{ asset('css/styles.css') }}">
 </head>
 
 <body>
@@ -65,12 +23,6 @@
                 <div class="buton">
                     <button>Ver mas info</button>
                 </div>
-                <p>
-
-                    <!-- Maspujols es un pequeño pueblo de la comarca del Baix Camp,
-                    en la provincia de Tarragona. Se encuentra a unos 20 kilómetros de Reus
-                    y a unos 50 kilómetros de Tarragona. -->
-                </p>
             </div>
         </div>
 
