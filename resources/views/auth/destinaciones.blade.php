0398fd04e0e418cf9298961b0524a7a45b436c52
jose-giithub <jose.rodriguez@insbaixcamp.cat>
2024-01-16 17:13:52 +0100

    Añado un boton de retorno al Dash

--- resources/views/auth/destinaciones.blade.php
+++ resources/views/auth/destinaciones.blade.php
@@ -4,17 +4,29 @@
 <head>
     <meta charset="UTF-8">
     <title>Detalle del Destino</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
+
     <style>
-        body{
+        body {
             background-color: #BBDEFB;
         }
+
         header {
+            padding-top: 50px;
             text-align: center;
             margin-bottom: 2rem;
-           
+
+        }
+
+        #botonRetornar {
+            margin-top: 20px;
+            margin-right: 20px;
+            display: block;
+            float: right;
         }
 
         .container {
+
             display: flex;
             justify-content: space-around;
             flex-wrap: wrap;
@@ -27,7 +39,7 @@
             border-radius: 4px;
             box-shadow: 0 2px 4px rgba(0, 0, 0, 0.1);
             overflow: hidden;
-            width: 300px;
+            width: 250px;
             /* Ancho fijo para cada tarjeta */
             height: 800px;
             /* Altura fija para cada tarjeta */
@@ -50,16 +62,16 @@
 </head>
 
 <body>
+    <button type="button" class="btn btn-primary" id="botonRetornar">
+        <a href="{{ route('dashboard') }}" class="btn btn-primary">Inicio</a>
+    </button>
     <header id="header">
-        <h1>Detalle del Destino</h1> 
-        <button>
-                <a href="{{ route('dashboard') }}" class="btn btn-primary">Retornar al dashboard</a>
-                </button>
+
+        <h1>Detalle del Destino</h1>
+
     </header>
 
     <div class="container">
-
-
 
         <div class="card">
             <img src="{{ asset('images/maspullols.jpg') }}" alt="Imagen de destino">
