900910c802f2d1d2853ad21b0e69e54a9cc717d3
jose-giithub <jose.rodriguez@insbaixcamp.cat>
2024-01-16 17:45:34 +0100

    Añado 6 destinos a la vista

--- resources/views/auth/destinaciones.blade.php
+++ resources/views/auth/destinaciones.blade.php
@@ -4,9 +4,9 @@
 <head>
     <meta charset="UTF-8">
     <title>Detalle del Destino</title>
-<<<<<<< HEAD
+
     <link rel="stylesheet" href="{{ asset('css/styles.css') }}">
-=======
+
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
 
     <style>
@@ -62,7 +62,7 @@
             text-align: center;
         }
     </style>
->>>>>>> 0398fd04e0e418cf9298961b0524a7a45b436c52
+
 </head>
 
 <body>
@@ -146,7 +146,41 @@
                 </p>
             </div>
         </div>
+        <div class="card">
+            <img src="{{ asset('images/cabarceno.jpg') }}" alt="Imagen de destino">
+            <div class="card-body">
+                <h5>Cabárceno</h5>
+                <p>
+                    Cabárceno , añadir text texto texto
+
+                </p>
+            </div>
+        </div>
+
+
+        <div class="card">
+            <img src="{{ asset('images/Gigantes.jpg') }}" alt="Imagen de destino">
+            <div class="card-body">
+                <h5>Los Gigantes</h5>
+                <p>
+                    Los Gigantes , añadir text texto texto
+                </p>
+            </div>
+        </div>
+        <div class="card">
+            <img src="{{ asset('images/Monfragüe.jpg') }}" alt="Imagen de destino">
+            <div class="card-body">
+                <h5>Parque Nacional de Monfragüe, Cáceres</h5>
+                <p>
+                Monfragüe , añadir text texto texto
+                </p>
+            </div>
+        </div>
+
+
     </div>
+
+
 
 </body>
 
